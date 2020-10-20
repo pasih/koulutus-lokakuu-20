7970f0d879ae3634fb06d619b5bfb1ab28114860
Pasi Hirvonen <pasi.hirvonen@visma.com>
2020-10-20 13:22:41 +0300

    Ensimmäinen tauko

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "octo-sample-api/styles.css"
 
+import { getPosts } from "octo-sample-api"
+import React, { useEffect, useState } from 'react';
+
+/*
+body: "quia et suscipit↵suscipit recusandae consequuntur expedita et cum↵reprehenderit molestiae ut ut quas totam↵nostrum rerum est autem sunt rem eveniet architecto"
+id: 1
+title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+userData: {id: 1, name: "Leanne Graham", username: "Bret", email: "Sincere@april.biz", address: {…}, …}
+userId: 1
+*/
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getPosts().then(loadedPosts => {
+      setPosts(loadedPosts);
+      setLoading(false)
+    })
+  }, [])
+  
+  // if (loading) {
+  //   return <div>Ladataan</div>
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="posts-app">
+      <div className="post-list">
+        {loading ? "Ladataan.." : 
+        <ul>
+          {posts.map(post => <li key={post.id}>
+            {post.title}
+          </li>)}
+        </ul>}
+      </div>
     </div>
   );
 }
